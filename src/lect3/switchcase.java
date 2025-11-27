2a3701e496fd394ea9022053c40f910b531035ee
shraddhamishra21 <shraddhamishra08444@gmail.com>
2025-11-27 20:24:27 +0530

    switchcase

---
+++ src/lect3/switchcase.java
@@ -0,0 +1,25 @@
+package lect3;
+import java.util.*;
+public class switchcase {
+    public static<Char> void main(String[] args) {
+   char ch='y';
+    switch(ch){
+        case 'a':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'e':
+        case 'A':
+        case 'E':
+        case 'O':
+        case 'U':
+        case 'I':
+
+            System.out.println("vowel");
+            break;
+        default:
+            System.out.println("consonent"); }
+}
+}
+
+
